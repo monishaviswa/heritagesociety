1c20e5020e1e8d927d2a255d3f691c846ed57f47
monisha <mviswa01@seattlecentral.edu>
2014-11-20 20:49:19 -0800

    Test Home Banner
    Home banner test 2

--- page-home.php
+++ page-home.php
@@ -2,6 +2,14 @@
 <!-- START CONTENT -->
 <div id="middle"><!-- START div middle -->
 
+<?php if (have_posts()) : ?>
+    <?php while (have_posts()) : the_post(); ?>
+  
+    	<h2> <?php the_title(); ?></h2>
+        <?php the_content('More &raquo;'); ?>
+    <?php endwhile; ?>
+
+<?php endif; ?>
 
 
 
